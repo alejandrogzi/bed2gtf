0616a0f0ccb3b7ca3e3550ffd8f7fb5ad04021b7
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2023-09-13 01:01:57 -0500

    BREAKING CHANGE: v.1.5.0 now outputs GTF3 format (5UTR -> five_prime_utr); new logging; impl tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
 use std::collections::{HashMap, HashSet};
-use std::io::{BufRead, BufReader, Write, Error};
+use std::io::{BufRead, BufReader, Write};
 use std::path::PathBuf;
 use std::fs::File;
 use std::cmp::{max, min};
+use std::time::Instant;
+use std::error::Error;
+
+use colored::Colorize;
+use peak_alloc::PeakAlloc;
+
+use log::Level;
+
+use indoc::indoc;
 
 mod bed;
 use bed::BedRecord;
@@ -13,11 +22,13 @@ use codon::Codon;
 
 const SOURCE: &str = "bed2gtf";
 
+#[global_allocator]
+static PEAK_ALLOC: PeakAlloc = PeakAlloc;
 
 
 /// Store the isoforms in a HashMap in the form: isoform -> gene.
 /// Provides a fast access to the gene name given an isoform name.
-fn get_isoforms(path: PathBuf) -> Result<HashMap<String, String>, Error> {
+fn get_isoforms(path: PathBuf) -> Result<HashMap<String, String>, Box<dyn Error>> {
     let file: File = File::open(path).unwrap();
     let reader: BufReader<File> = BufReader::new(file);
     let mut isoforms: HashMap<String, String> = HashMap::new();
@@ -269,7 +280,7 @@ fn write_features(i: usize, record: &BedRecord, gene_name: &str, first_utr_end: 
 
     if exon_start < first_utr_end {
         let end = min(exon_end, first_utr_end);
-        let utr_type = if record.strand() == "+" { "5UTR" } else { "3UTR" };
+        let utr_type = if record.strand() == "+" { "five_prime_utr" } else { "three_prime_utr" };
         build_gtf_line(record, gene_name, utr_type, exon_start, end, frame, -1, file);
     }
 
@@ -281,7 +292,7 @@ fn write_features(i: usize, record: &BedRecord, gene_name: &str, first_utr_end: 
 
     if exon_end > last_utr_start {
         let start = max(exon_start, last_utr_start);
-        let utr_type = if record.strand() == "+" { "3UTR" } else { "5UTR" };
+        let utr_type = if record.strand() == "+" { "three_prime_utr" } else { "five_prime_utr" };
         build_gtf_line(record, gene_name, utr_type, start, exon_end, frame, -1, file);
     }
 }
@@ -357,7 +368,12 @@ fn to_gtf(record: &BedRecord, isoforms: &HashMap<String, String>, file: &mut Fil
 /// use bed2gtf::bed2gtf;
 /// bed2gtf("input.bed", "isoforms.txt", "output.gtf");
 /// ```
-pub fn bed2gtf(input: &String, isoforms: &String, output: &String) {
+pub fn bed2gtf(input: &String, isoforms: &String, output: &String) -> Result<(), Box<dyn Error>> {
+
+    msg();
+    simple_logger::init_with_level(Level::Info)?;
+
+    let start = Instant::now();
     let bedfile = File::open(PathBuf::from(input)).unwrap();
     let reader = BufReader::new(bedfile);
     
@@ -366,7 +382,7 @@ pub fn bed2gtf(input: &String, isoforms: &String, output: &String) {
     let mut seen_genes: HashSet<String> = HashSet::new();
 
     for line in reader.lines() {
-        let line = line.unwrap();
+        let line = line?;
         let fields: Vec<&str> = line.split('\t').collect();
         let record = BedRecord::new(fields);
 
@@ -376,6 +392,80 @@ pub fn bed2gtf(input: &String, isoforms: &String, output: &String) {
             let _ = to_gtf(&record, &isoforms, &mut output, true);
         } else {
             let _ = to_gtf(&record, &isoforms, &mut output, false);
+        }
+    }
+
+    let peak_mem = PEAK_ALLOC.peak_usage_as_mb();
+
+    log::info!("Memory usage: {} MB", peak_mem);
+    log::info!("Elapsed: {:.4?}", start.elapsed().as_secs_f32());
+
+    Ok(())
+}
+
+
+
+fn msg() {
+    println!("{}\n{}",
+        "\n##### BED2GTF #####".bright_cyan().bold(),
+        indoc!("A fast BED-to-GTF converter written in Rust.
+        Repository: https://github.com/alejandrogzi/bed2gtf
+        Feel free to contact the developer if any issue/suggest/bug is found.
+        "));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Read;
+
+    fn make_bed_test_file(content: &str) -> Result<String, Box<dyn Error>> {
+        let mut file = File::create("test.bed")?;
+        file.write_all(content.as_bytes())?;
+
+        Ok("test.bed".to_string())
+    }
+
+    fn make_isoforms_test_file(content: &str) -> Result<String, Box<dyn Error>> {
+        let mut file = File::create("test.isoforms")?;
+        file.write_all(content.as_bytes())?;
+
+        Ok("test.isoforms".to_string())
+    }
+
+    #[test]
+    fn main_test() {
+        let input = make_bed_test_file("chr15\t81000922\t81005788\tENST00000267984\t0\t+\t81002271\t81003360\t0\t1\t4866,\t0,").unwrap();
+
+        let isoforms = make_isoforms_test_file(indoc!("ENSG00000140406\tENST00000267984
+        ENSG00000140545\tENST00000560937
+        ENSG00000082438\tENST00000495084")).unwrap();
+
+        let output = "out.gtf".to_string();
+
+        let _ = bed2gtf(&input, &isoforms, &output).unwrap();
+        let mut file = File::open(output.clone()).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let converted_content = indoc!("chr15\tbed2gtf\tgene\t81000923\t81005788\t.\t+\t.\tgene_id \"ENSG00000140406\";
+        chr15\tbed2gtf\ttranscript\t81000923\t81005788\t.\t+\t.\tgene_id \"ENSG00000140406\"; transcript_id \"ENST00000267984\";
+        chr15\tbed2gtf\texon\t81000923	81005788\t.\t+\t.\tgene_id \"ENSG00000140406\"; transcript_id \"ENST00000267984\"; exon_number \"1\"; exon_id \"ENST00000267984.1\";
+        chr15\tbed2gtf\tfive_prime_utr\t81000923\t81002271\t.\t+\t0\tgene_id \"ENSG00000140406\"; transcript_id \"ENST00000267984\";
+        chr15\tbed2gtf\tCDS	81002272\t81003357\t.\t+\t0\tgene_id \"ENSG00000140406\"; transcript_id \"ENST00000267984\"; exon_number \"1\"; exon_id \"ENST00000267984.1\";
+        chr15\tbed2gtf\tthree_prime_utr\t81003361\t81005788\t.\t+\t0\tgene_id \"ENSG00000140406\"; transcript_id \"ENST00000267984\";
+        chr15\tbed2gtf\tstart_codon\t81002272\t81002274\t.\t+\t0\tgene_id \"ENSG00000140406\"; transcript_id \"ENST00000267984\"; exon_number \"1\"; exon_id \"ENST00000267984.1\";
+        chr15\tbed2gtf\tstop_codon\t81003358\t81003360\t.\t+\t0\tgene_id \"ENSG00000140406\"; transcript_id \"ENST00000267984\"; exon_number \"1\"; exon_id \"ENST00000267984.1\";\n");
+
+        assert_eq!(contents, converted_content);
+
+        teardown(vec![input, isoforms, output]);
+    }
+
+    fn teardown(files: Vec<String>) {
+        for file in files {
+            std::fs::remove_file(file).unwrap();
         }
     }
 }
