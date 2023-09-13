6ac45d0daf7f016f2149eda3d070830c6cf329f8
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2023-09-13 01:03:18 -0500

    new(test): add basic test modules

--- src/bed.rs
+++ src/bed.rs
@@ -123,3 +123,62 @@ impl BedRecord {
     }
 
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_record() {
+        let fields = vec![
+            "chr1",
+            "11869",
+            "14409",
+            "DDX11L1",
+            "0",
+            "+",
+            "11869",
+            "11869",
+            "0",
+            "3",
+            "354,120,247",
+            "11869,12612,13220",
+        ];
+
+        let record = BedRecord::new(fields);
+
+        assert_eq!(record.chrom(), "chr1");
+        assert_eq!(record.tx_start(), 11869);
+        assert_eq!(record.tx_end(), 14409);
+        assert_eq!(record.name(), "DDX11L1");
+        assert_eq!(record.strand(), "+");
+        assert_eq!(record.cds_start(), 11869);
+        assert_eq!(record.cds_end(), 11869);
+        assert_eq!(record.exon_count(), 3);
+        assert_eq!(record.exon_start(), &vec![23738, 24481, 25089]);
+        assert_eq!(record.exon_end(), &vec![24092, 24601, 25336]);
+    }
+
+    #[test]
+    fn get_exon_frames() {
+        let fields = vec![
+            "chr1",
+            "164680187",
+            "164841823",
+            "ABC",
+            "0",
+            "+",
+            "164685945",
+            "164841196",
+            "0",
+            "14",
+            "5845,177,1568,95,235,75,154,237,98,229,202,189,91,114",
+            "0,12033,14081,19217,20334,24256,24764,41887,42237,47781,49726,63499,160968,161522",
+        ];
+
+        let record = BedRecord::new(fields);
+
+        assert_eq!(record.get_exon_frames(), vec![0, 0, 0, 2, 1, 2, 2, 0, 0, 2, 0, 1, 1, -1]);
+    }
+}
